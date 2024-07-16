78c6ad6a41b4e5856c088888118e9d4e6c800501
Luis Miguel Gonzalez <luismimultimedia@gmail.com>
2024-07-15 23:47:03 -0500

    cors again

--- src/index.js
+++ src/index.js
@@ -6,18 +6,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Pass to next layer of middleware
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
     next();
-});
+  });
 
 // Ruta para obtener los datos de los productos
 app.get('/api/product-list', (req, res) => {
