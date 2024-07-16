0b24bb1c2a864c9a43dc49dcb65d4df6ddd509c8
Luis Miguel Gonzalez <luismimultimedia@gmail.com>
2024-07-15 23:40:20 -0500

    Another try setting cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
-const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Configuración de CORS
-app.use(cors())
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // Ruta para obtener los datos de los productos
 app.get('/api/product-list', (req, res) => {
