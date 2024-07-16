e413fa5476cdbcf5ea757fef0cd10871841547a8
Luis Miguel Gonzalez <luismimultimedia@gmail.com>
2024-07-15 23:59:56 -0500

    anothe one

--- src/index.js
+++ src/index.js
@@ -5,11 +5,25 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Add headers
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next();
-  });
+});
 
 // Ruta para obtener los datos de los productos
 app.get('/api/product-list', (req, res) => {
